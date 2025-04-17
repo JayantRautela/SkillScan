6de3a640192911315f0d68b997dcae980d393077
Jayant Singh Rautela <jayantrautela0205@gmail.com>
2025-04-17 23:36:28 +0530

    add routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
 import Home from "./pages/Home"
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import Signup from "./pages/Signup"
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />
+  },
+  {
+    path: '/signup',
+    element: <Signup />
+  }
+]);
 
 function App() {
 
   return (
     <>
-      <Home />
+      <RouterProvider router={router} />
     </>
   )
 }
--- src/components/shared/Navbar.tsx
+++ src/components/shared/Navbar.tsx
@@ -1,19 +1,25 @@
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import { Button } from '../ui/button';
 
 const Navbar = () => {
+  const navigate = useNavigate();
+
+  const clickHandler = () => {
+    navigate('/signup')
+  }
+
   return (
     <nav className="h-16 w-full flex items-center justify-between px-10 bg-black text-white">
-      <div className="tracking-tighter text-lg">
+      <Link to='/' className="tracking-tighter text-lg cursor-pointer">
         SkillScan
-      </div>
+      </Link>
       <div className='flex gap-10'>
         <Link to='/'>Resume Analysis</Link>
         <Link to='/about'>Learning Path</Link>
         <Link to='/contact'>Success Stories</Link>
       </div>
       <div>
-        <Button className='bg-blue-500 px-6 cursor-pointer hover:bg-blue-600'>Sign Up</Button>
+        <Button className='bg-blue-500 px-6 cursor-pointer hover:bg-blue-600' onClick={clickHandler}>Sign Up</Button>
       </div>
     </nav>
   )
