24481374a7d2b4959dee8bc19606b67b9c2bbbeb
Jayant Singh Rautela <jayantrautela0205@gmail.com>
2025-04-19 22:28:42 +0530

    add login route

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import Home from "./pages/Home"
 import { createBrowserRouter, RouterProvider } from "react-router-dom"
 import Signup from "./pages/Signup"
 import { Toaster } from "./components/ui/sonner";
+import Login from "./pages/Login";
 
 const router = createBrowserRouter([
   {
@@ -11,6 +12,10 @@ const router = createBrowserRouter([
   {
     path: '/signup',
     element: <Signup />
+  },
+  {
+    path: '/login',
+    element: <Login />
   }
 ]);
 
