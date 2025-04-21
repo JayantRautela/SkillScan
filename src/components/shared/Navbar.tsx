b73b3759d0b6f02d2e30a557a2ce1470dd0c54b2
Jayant Singh Rautela <jayantrautela0205@gmail.com>
2025-04-21 22:10:18 +0530

    add responsiveness to navbar

--- src/components/shared/Navbar.tsx
+++ src/components/shared/Navbar.tsx
@@ -1,7 +1,11 @@
 import { Link, useNavigate } from 'react-router-dom'
 import { Button } from '../ui/button';
+import { useState } from 'react';
+import { Menu, X } from 'lucide-react';
 
 const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   const navigate = useNavigate();
 
   const clickHandler = () => {
@@ -9,18 +13,39 @@ const Navbar = () => {
   }
 
   return (
-    <nav className="h-16 w-full flex items-center justify-between px-10 bg-black text-white">
+    <nav className="h-16 w-full flex items-center justify-between px-6 bg-black text-white relative md:px-10">
       <Link to='/' className="tracking-tighter text-lg cursor-pointer">
         SkillScan
       </Link>
-      <div className='flex gap-10'>
+      <div className='hidden md:flex gap-10'>
         <Link to='/'>Resume Analysis</Link>
         <Link to='/about'>Learning Path</Link>
         <Link to='/contact'>Success Stories</Link>
       </div>
-      <div>
+      <div className="md:hidden">
+        <button onClick={() => setMenuOpen(prev => !prev)}>
+          {menuOpen ? <X className="cursor-pointer w-6 h-6" /> : <Menu className="w-6 h-6 cursor-pointer" />}
+        </button>
+      </div>
+      <div className="hidden md:block">
         <Button className='bg-blue-500 px-6 cursor-pointer hover:bg-blue-600' onClick={clickHandler}>Sign Up</Button>
       </div>
+      {menuOpen && (
+        <div className="absolute top-16 left-0 w-full bg-black text-white flex flex-col items-center gap-4 py-4 md:hidden z-10">
+          <Link to='/' onClick={() => setMenuOpen(false)}>Resume Analysis</Link>
+          <Link to='/about' onClick={() => setMenuOpen(false)}>Learning Path</Link>
+          <Link to='/contact' onClick={() => setMenuOpen(false)}>Success Stories</Link>
+          <Button
+            className="bg-blue-500 px-6 cursor-pointer hover:bg-blue-600"
+            onClick={() => {
+              setMenuOpen(false)
+              clickHandler()
+            }}
+          >
+            Sign Up
+          </Button>
+        </div>
+      )}
     </nav>
   )
 }
