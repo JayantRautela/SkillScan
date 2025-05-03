c7f09056f74cb35c55388ec41f60be36e52088d2
Jayant Singh Rautela <jayantrautela0205@gmail.com>
2025-05-04 01:27:52 +0530

    add logoutHandler to navbar component

--- src/components/shared/Navbar.tsx
+++ src/components/shared/Navbar.tsx
@@ -1,20 +1,53 @@
 import { Link, useNavigate } from 'react-router-dom'
 import { Button } from '../ui/button';
 import { useState } from 'react';
-import { Menu, X } from 'lucide-react';
+import { CheckCircle, Menu, X, XCircle } from 'lucide-react';
 import { useSelector } from 'react-redux';
 import { Popover, PopoverContent, PopoverTrigger } from '../ui/popover';
 import { Avatar, AvatarFallback, AvatarImage } from '../ui/avatar';
+import { useDispatch } from 'react-redux';
+import { setUser } from '@/redux/authSlice';
+import { toast } from 'sonner';
+import { AppDispatch } from '@/redux/store';
+
+interface ServerResponse {
+  status: number;
+  message: string;
+}
 
 const Navbar = () => {
   const [menuOpen, setMenuOpen] = useState(false);
   const { user } = useSelector((store: any) => store.auth);
+  const dispatch = useDispatch<AppDispatch>();
 
   const navigate = useNavigate();
 
   const clickHandler = () => {
     navigate('/signup')
   }
+
+  const logoutHandler = async () => {
+    try {
+      const res = await axios.get<ServerResponse>(`http://localhoat:2000/logout`, {
+        withCredentials: true,
+      });
+      if (res.status === 200) {
+        dispatch(setUser(null));
+        navigate("/home");
+        toast.success(res.data.message, {
+          icon: <CheckCircle className="text-green-600 w-5 h-5" />,
+        });
+      }
+    } catch (error: any) {
+        const message =
+          error?.response?.data?.message ||
+          error?.message ||
+          "Some error occurred";
+        toast.error(message, {
+          icon: <XCircle className="text-red-600 w-5 h-5" />,
+        });
+      }
+  };
 
   return (
     <nav className="h-16 w-full flex items-center justify-between px-6 bg-black text-white relative md:px-10">
@@ -43,12 +76,17 @@ const Navbar = () => {
               </Avatar>
             </PopoverTrigger>
             <PopoverContent className="w-80">
-              <div className="flex gap-4 space-y-2">
-                <Avatar>
-                  <AvatarImage src={user?.profilePicture} />
-                </Avatar>
+              <div className="flex gap-4 space-y-2 flex-col">
+                <div className="flex gap-4">
+                  <Avatar>
+                    <AvatarImage src={user?.profilePicture} />
+                  </Avatar>
+                  <div>
+                    <h4 className="font-medium">{user?.username}</h4>
+                  </div>
+                </div>
                 <div>
-                  <h4 className="font-medium">{user?.username}</h4>
+                  <Button className='cursor-pointer' onClick={logoutHandler}>LogOut</Button>
                 </div>
               </div>
             </PopoverContent>
